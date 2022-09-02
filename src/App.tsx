db6a7268a952c9683d65e0dd43ea9dccb6ebbfa1
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2022-09-02 14:52:25 +0500

    initial commit data implement to front

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Products} from './components/Products'
+import {products} from './components/data/products'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto max-w-2xl pt-5'>
+
+      {
+        products.map((product) => <Products product={product} key={product.id}/>)
+      }
+
+      {/* <Products product={products[0]}/>  */}
+      {/* <Products product={products[1]}/>  */}
     </div>
-  );
+  )
 }
 
 export default App;
